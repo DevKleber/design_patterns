16eece27cd1306d34cdf5e8416cdcd0e07f70bcf
Kleber de Souza <kleber.souza@hiplatform.com>
2023-04-21 19:53:36 -0300

    pattern => Template Method

---
+++ src/Impostos/Icpp.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Src\Impostos;
+
+use Src\Orcamento;
+
+class Icpp extends ImpostoCom2Aliquotas
+{
+    protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool
+    {
+        return $orcamento->valor > 500;
+    }
+
+    protected function calculaTaxaMaxima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.03;
+    }
+
+    protected function calculaTaxaMinima(Orcamento $orcamento): float
+    {
+        return $orcamento->valor * 0.02;
+    }
+}
---
+++ src/Impostos/ImpostoCom2Aliquotas.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Src\Impostos;
+
+use Src\Orcamento;
+
+abstract class ImpostoCom2Aliquotas implements Imposto
+{
+    public function calculaImposto(Orcamento $orcamento): float
+    {
+        if ($this->deveAplicarTaxaMaxima($orcamento)) {
+            return $this->calculaTaxaMaxima($orcamento);
+        }
+
+        return $this->calculaTaxaMinima($orcamento);
+    }
+
+    abstract protected function deveAplicarTaxaMaxima(Orcamento $orcamento): bool;
+
+    abstract protected function calculaTaxaMaxima(Orcamento $orcamento): float;
+
+    abstract protected function calculaTaxaMinima(Orcamento $orcamento): float;
+}
